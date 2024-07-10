cfbc88441dd97e7c6dc12008770bdc8e1940cd52
juangiraldo95 <juanfe-0701@hotmail.com>
2024-07-10 17:54:13 -0500

    se agrego la logica de envio de correo

--- Controllers/RecoverPassword/RecoverPasswordController.cs
+++ Controllers/RecoverPassword/RecoverPasswordController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using GHV.Data;
+using GHV.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -11,46 +12,52 @@ using Microsoft.Extensions.Logging;
 namespace GHV.Controllers
 {
     [Route("[controller]")]
-    public class RecuperaContrasenaController : Controller
+    public class RecoverPasswordController : Controller
     {
         private readonly BaseContext _context;
 
-        public RecuperaContrasenaController(BaseContext context)
+        public RecoverPasswordController(BaseContext context)
         {
             _context = context;
         }
 
-        public IActionResult RecuperaContrasena()
+        public IActionResult RecoverPassword()
         {
             return View();
         }
 
         
         [HttpPost]
-        public async Task<IActionResult> RecuperaContrasena(string email)
+        public async Task<IActionResult> RecoverPassword(string email)
         {
+
+            
             email = "danitorresm734@gmail.com";
             if (string.IsNullOrEmpty(email))
             {
                 ViewData["Mensaje"] = "Por favor ingrese un correo electrónico válido.";
-                return View();
             }
           
             var usuario = await  _context.Usuarios.FirstOrDefaultAsync(u => u.Email == email);
+
+
             if (usuario == null)
             {
                 
-                 
                 ViewData["Mensaje"] = $"No se encontró ningún usuario registrado con el correo electrónico {email}.";
-                return RedirectToAction("Home", "Privacy");
-                
+
             }
             /*HttpContext.Session.SetString("Names", usuario.Nombre);*/
             // Aquí deberías implementar la lógica para enviar el correo de recuperación
             // Simulamos enviando un mensaje de éxito
             ViewData["Mensaje"] = $"Se ha enviado un correo de recuperación a {email}";
 
-            return View("Home","Index");
+            MailController Email = new MailController();
+            Email.EnviarCorreo(usuario);
+
+            return View("Index");
+
+
         }
     }
 }
