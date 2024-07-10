c135564a9221fc16e7b4eefcd24f1f21f36b7389
juangiraldo95 <juanfe-0701@hotmail.com>
2024-07-10 18:08:06 -0500

    agregue la logica para crear usuario

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -1,12 +1,43 @@
 using Microsoft.AspNetCore.Mvc;
+using GHV.Models;
+using GHV.Data;
+using Microsoft.AspNetCore.Authentication;
 
 namespace GHV.Controllers
 {
     public class RegisterController : Controller
     {
+        public readonly BaseContext _context;
+
+        public RegisterController (BaseContext context)
+        {
+            _context = context;
+        }
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Usuarios.Add(usuario);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index", "Home");
+                }
+                catch (Exception)
+                {
+                    // Manejar errores aquí, por ejemplo, loggear el error y mostrar un mensaje de error al usuario
+                    ModelState.AddModelError("", "Error al intentar registrar el usuario. Por favor, intenta nuevamente.");
+                    return View(); 
+                }
+            }
+            return View(usuario); // Retornar la vista con el modelo de usuario si el modelo no es válido
+        }
+        
     }
 }
